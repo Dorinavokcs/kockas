a599997c838a007295f70b19e28d1c1a5c7b1387
l.cs.dorina <l.cs.dorina@gmail.com>
2023-11-15 13:50:33 +0100

    13:50

---
+++ ize.js
@@ -0,0 +1,28 @@
+const matrix = document.querySelector(".matrix");
+function render(){
+    matrix.innerHTML = "";
+    for(let i = 0; i <25; i++){   const field =document.createElement("div");
+    //esemeny figyelo
+    field.addEventListener("click", coloring)
+    field.classList.add("field");
+    matrix.appendChild(field)
+    }
+}
+function RandomColoring(){
+    return Math.floor(Math.random()*254+1)
+
+
+
+
+}
+
+function coloring(){
+    let r = randomColor();
+    let g= randomColor();
+    let b = randomColor();
+
+
+}
+
+
+render();
